5df29e901e49ba5fbe259d3de446ee6c94813ccf
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-15 18:33:25 +0200

    Add Provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+// --- inside-app imports -----------------------
 import * as serviceWorker from "./serviceWorker";
+import store from "./store";
 
-render(<h1>Inicjalizacja projektu</h1>, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <h1>Inicjalizacja projektu</h1>
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
